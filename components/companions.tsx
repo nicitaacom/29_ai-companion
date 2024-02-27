2ce2eec6f56a6517534157f64e610cfad68cb247
Nikita <nicitaacom@gmail.com>
2024-02-27 18:07:31 +0100

    +upd: companion examples

--- components/companions.tsx
+++ components/companions.tsx
@@ -20,6 +20,7 @@ export function Companions({ data, messages }: CompanionsProps) {
       </div>
     )
   }
+
   return (
     <div
       className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2 pb-10"
