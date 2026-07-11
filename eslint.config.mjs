533905250c18d734c81d2ebc34a0e4cb32e51aed
Nikita <nicitaacom@gmail.com>
2026-07-11 15:46:59 +0200

    chore: eslint no-export-classname

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -31,6 +31,7 @@ export default [
       "unicorn/catch-error-name": "warn",
       "max-len": ["warn", { code: 130, ignoreUrls: true }],
       "import/newline-after-import": ["warn", { count: 1 }],
+      "local-rules/no-export-const-classname": "warn",
     },
   },
 ]
