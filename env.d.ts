600c6dd36ce131e1506f2b637478b18a23e674cf
Nikita <nicitaacom@gmail.com>
2026-07-11 18:25:28 +0200

    upd: env.d.ts with updated envs

--- env.d.ts
+++ env.d.ts
@@ -32,6 +32,13 @@ declare global {
 
       STRIPE_WEBHOOK_SECRET: string
       STRIPE_SECRET_KEY: string
+
+      NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: string
+      STRIPE_SECRET_KEY: string
+
+      AWS_ACCESS_KEY_ID: string
+      AWS_SECRET_ACCESS_KEY: string
+      NEXT_PUBLIC_AWS_REGION: string
     }
   }
 }
