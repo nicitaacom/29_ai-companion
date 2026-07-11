d936c91085f244152d2fa31f4b182c8252328189
Nikita <nicitaacom@gmail.com>
2026-07-11 16:11:56 +0200

    chore: eslint input-value-naming

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -40,6 +40,7 @@ export default [
       "local-rules/sdk-method-naming": "warn",
       "local-rules/no-throwaway-alias": "warn",
       "local-rules/no-zustand-types-in-store-file": "warn",
+      "local-rules/input-value-naming": "warn",
     },
   },
 ]
