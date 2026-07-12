db7a6e6938ef97ddaf3ed3f69e30447db19f20bf
Nikita <nicitaacom@gmail.com>
2026-07-12 13:52:34 +0200

    Revert "chore: rate limit add generateMedia"
    This reverts commit b51784e6025173e2d356537f072e83f264141465.

--- app/consts/RATE_LIMIT.ts
+++ app/consts/RATE_LIMIT.ts
@@ -17,12 +17,4 @@ export const RATE_LIMITS = {
       return `chat:new-message:${userId}`
     },
   },
-  generateMedia: {
-    windowSec: 3600,
-    maxAllowed: 10,
-    key: ({ userId }: RateLimitKeyParams) => {
-      if (!userId) throw new Error("userId is required for generateMedia")
-      return `generate:media:${userId}`
-    },
-  },
 } as const
